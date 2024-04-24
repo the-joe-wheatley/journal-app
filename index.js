0ce6ab5a0ac767492d65fd67f4ca806f5b41aa77
Joseph Wheatley <info@wheatleydigital.com>
2024-04-24 08:26:29 +0100

    Updated to cookie session

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import session from "express-session";
+import session from "cookie-session";
 import "dotenv/config";
 import pg from "pg";
 import bcrypt from "bcrypt";
